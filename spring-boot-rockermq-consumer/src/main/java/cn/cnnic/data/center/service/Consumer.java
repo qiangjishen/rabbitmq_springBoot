980c7102081c6bd6412b961181cb23c785f4bbba
qiangjishen <qiangjishen@gmail.com>
2023-06-09 20:48:48 +0800

    更新实际MQ应用场景及问题

--- spring-boot-rockermq-consumer/src/main/java/cn/cnnic/data/center/service/Consumer.java
+++ spring-boot-rockermq-consumer/src/main/java/cn/cnnic/data/center/service/Consumer.java
@@ -1,16 +1,29 @@
 package cn.cnnic.data.center.service;
 
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.apache.rocketmq.spring.core.RocketMQPushConsumerLifecycleListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RocketMQMessageListener(topic = "sdnsd-topic",consumerGroup = "my-consumer-group",consumeMode = ConsumeMode.CONCURRENTLY, messageModel = MessageModel.CLUSTERING,  selectorExpression = "*")
-public class Consumer implements RocketMQListener<String> {
+public class Consumer implements RocketMQListener<MessageExt>, RocketMQPushConsumerLifecycleListener {
     @Override
-    public void onMessage(String message) {
+    public void onMessage(MessageExt message) {
         System.out.println("消费者收到信息：==="+message);
     }
+
+    //    该方法重写消息监听器的属性
+    @Override
+    public void prepareStart(DefaultMQPushConsumer defaultMQPushConsumer) {
+        // 设置消费者消息重试次数
+        defaultMQPushConsumer.setMaxReconsumeTimes(3);
+        //        设置实例名称
+        defaultMQPushConsumer.setInstanceName("mqconsumer1");
+    }
+
 }
